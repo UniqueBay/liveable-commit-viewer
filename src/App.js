5b2c07f0a69f9ec72d3cb0b6321fd313fe0b1edf
Alabi Abayomi <abayomi15@gmail.com>
2022-02-19 13:09:50 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <h1> Am here </h1>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
